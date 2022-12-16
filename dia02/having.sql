e32a569b3dc8cd3a79cef040b3ce3f4d3647b0fc
rlrocha <rlrocha>
2022-12-16 01:17:30 +0000

    Add having.sql file

---
+++ dia02/having.sql
@@ -0,0 +1,16 @@
+-- Databricks notebook source
+SELECT descUF,
+       COUNT(idVendedor) AS qtVendedorUF
+
+FROM silver_olist.vendedor
+
+WHERE descUF in ('SP', 'MG', 'RJ', 'ES') -- sempre depois do from e antes do group by
+-- é onde a gente passa o sarrafo
+
+GROUP BY descUF
+
+-- HAVING COUNT(idVendedor) > 100 -- filtro depois da agregação
+HAVING qtVendedorUF > 100 -- filtro depois da agregação
+-- aparar a grama
+
+ORDER BY qtVendedorUF DESC
