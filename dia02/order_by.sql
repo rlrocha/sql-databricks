fa36e29ecf2ac2cd18aad80e20e136050ab3979e
rlrocha <rlrocha>
2022-12-16 19:04:30 +0000

    Update order_by.sql file

--- dia02/order_by.sql
+++ dia02/order_by.sql
@@ -33,3 +33,42 @@ GROUP BY descUF
 ORDER BY qtClienteEstado DESC -- depois do campo (order by)
 
 LIMIT 1 -- apenas uma linha
+
+-- COMMAND ----------
+
+-- MAGIC % python
+-- MAGIC 
+-- MAGIC data = [
+-- MAGIC     
+-- MAGIC     "nome": "teo", "idade": 30,
+-- MAGIC     "nome": "rodrigo", "idade": 327,
+-- MAGIC     "nome": "victor", "idade":20,
+-- MAGIC     "nome": "gabriel", "idade":28,
+-- MAGIC     "nome": "carlos", "idade":27,
+-- MAGIC     "nome": "josé", "idade":24,
+-- MAGIC     "nome": "fernando", "idade":34,
+-- MAGIC     "nome": "bruno", "idade":37,
+-- MAGIC     "nome": "patrick", "idade":31,
+-- MAGIC     "nome": "dodo", "idade":31,
+-- MAGIC     "nome": "alessa", "idade":31,
+-- MAGIC     
+-- MAGIC ]
+-- MAGIC 
+-- MAGIC import pandas as pd
+-- MAGIC 
+-- MAGIC -- df = pd.DataFrame(data)
+-- MAGIC -- df.head()
+-- MAGIC 
+-- MAGIC df = spark.createDataFrame(pd.DataFrame(data))
+-- MAGIC df.display()
+-- MAGIC 
+-- MAGIC df.createTempView("fodase")
+
+-- COMMAND ----------
+
+SELECT *
+
+FROM fodase
+
+-- não precisa estar com o group by
+ORDER BY idade DESC, nome ASC -- começa pela idade, e o critério de desempate é o nome
